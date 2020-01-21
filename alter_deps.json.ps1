6ca46c6db132324c42d9321644f30d82e452293b
klalle <kalle.lofgren@gmail.com>
2020-01-21 20:28:41 +0100

    upd

--- alter_deps.json.ps1
+++ alter_deps.json.ps1
@@ -1,22 +1,21 @@
 #param([string]$BuildPath)
 [IO.Directory]::SetCurrentDirectory((Convert-Path (Get-Location -PSProvider FileSystem)))
+
+#x86
 $filePath = "C:\.....path_to_your_project_referenced_by_UWP.......\obj\x86\Release\netcoreapp3.0\win-x86\Msix\WinSiftCore.deps.json"
 $filetxt = [IO.File]::ReadAllText($filePath)
-
 $regStr = "(?ms)(,[^}]*pdb.*?})"
 if ($filetxt -match $regStr) { 
     $matches[0]
-    $filetxt = ($filetxt -replace "(?ms)(,[^}]*pdb.*?})", "")
+    $filetxt = ($filetxt -replace $regStr, "")
     Set-Content -Path "$filePath" -Value $filetxt
 }
 
-
+#x64
 $filePath = "C:\.....path_to_your_project_referenced_by_UWP.......\obj\x64\Release\netcoreapp3.0\win-x64\Msix\WinSiftCore.deps.json"
 $filetxt = [IO.File]::ReadAllText($filePath)
-
-$regStr = "(?ms)(,[^}]*pdb.*?})"
 if ($filetxt -match $regStr) { 
     $matches[0]
-    $filetxt = ($filetxt -replace "(?ms)(,[^}]*pdb.*?})", "")
+    $filetxt = ($filetxt -replace $regStr, "")
     Set-Content -Path "$filePath" -Value $filetxt
 }
